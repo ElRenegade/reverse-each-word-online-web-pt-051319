bc0b49ef36ee83c590b47743612987c228dc57d8
Santiago Sanchez <chagostwo@yahoo.com>
2019-05-22 03:07:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(string)
-  new_array = string.split(" ") 
+  new_array = string.split(" ")
   final_array = []
   new_array.collect do|string|
-    final_array << string.reverse 
+    final_array << string.reverse
   end
   final_array.join(" ")
 
